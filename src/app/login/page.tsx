f42c503e2ee9107172db1b447cea37bcb33d777d
Daniel Mai <danielmai1728@gmail.com>
2024-04-05 16:34:01 -0700

    add login form

---
+++ src/app/login/page.tsx
@@ -0,0 +1,101 @@
+'use client'
+
+import { useToggle, upperFirst } from '@mantine/hooks'
+import { useForm } from '@mantine/form'
+import {
+  TextInput,
+  PasswordInput,
+  Text,
+  Paper,
+  Group,
+  PaperProps,
+  Button,
+  Checkbox,
+  Anchor,
+  Stack,
+  Center
+} from '@mantine/core'
+
+const Login = (props: PaperProps) => {
+  const [type, toggle] = useToggle(['login', 'register'])
+  const form = useForm({
+    initialValues: {
+      email: '',
+      name: '',
+      password: '',
+      terms: false
+    },
+
+    validate: {
+      email: (val: string) => (/^\S+@\S+$/.test(val) ? null : 'Invalid email'),
+      password: (val: string) => (val.length <= 6 ? 'Password should include at least 6 characters' : null)
+    }
+  })
+
+  return (
+    <Center className="h-screen">
+      <Paper radius="md" p="xl" withBorder>
+        <Text size="lg" fw={500} pb={5}>
+          {upperFirst(type)}
+        </Text>
+        <form
+          onSubmit={form.onSubmit((values) => {
+            console.log('subbmited ', values)
+          })}
+        >
+          <Stack>
+            {type === 'register' && (
+              <TextInput
+                label="Name"
+                placeholder="Your name"
+                value={form.values.name}
+                onChange={(event) => form.setFieldValue('name', event.currentTarget.value)}
+                radius="md"
+              />
+            )}
+
+            <TextInput
+              required
+              label="Email"
+              placeholder="hello@mantine.dev"
+              value={form.values.email}
+              onChange={(event) => form.setFieldValue('email', event.currentTarget.value)}
+              error={form.errors.email && 'Invalid email'}
+              radius="md"
+            />
+
+            <PasswordInput
+              required
+              label="Password"
+              placeholder="Your password"
+              value={form.values.password}
+              onChange={(event) => form.setFieldValue('password', event.currentTarget.value)}
+              error={form.errors.password && 'Password should include at least 6 characters'}
+              radius="md"
+            />
+
+            {type === 'register' && (
+              <Checkbox
+                required
+                label="I accept terms and conditions"
+                checked={form.values.terms}
+                onChange={(event) => form.setFieldValue('terms', event.currentTarget.checked)}
+              />
+            )}
+          </Stack>
+
+          <Group justify="space-between" mt="xl">
+            <Anchor component="button" type="button" c="dimmed" onClick={() => toggle()} size="xs">
+              {type === 'register' ? 'Already have an account? Login' : "Don't have an account? Register"}
+            </Anchor>
+            <Button type="submit" radius="xl">
+              {upperFirst(type)}
+            </Button>
+          </Group>
+        </form>
+      </Paper>
+    </Center>
+  )
+}
+
+export default Login
