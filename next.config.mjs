c175d4d84310b5c35484429dcf90f6519477980f
Daniel Mai <danielmai1728@gmail.com>
2024-04-05 17:41:26 -0700

    skip trailing slash redirect

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,8 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   output: 'export',
-  trailingSlash: true
+  trailingSlash: true,
+  skipTrailingSlashRedirect: true
 }
 
 export default nextConfig
