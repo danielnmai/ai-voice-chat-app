fed36f0d5c5d1e7894217df1a70b8c034fb7b7dd
Daniel Mai <danielmai1728@gmail.com>
2024-06-07 18:29:11 -0700

    remove unneeded image

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,7 +11,7 @@ const config: Config = {
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic': 'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
-        hero: "linear-gradient(250deg, rgba(130, 201, 30, 0) 0%, #062343 70%), url('/landing-1.jpeg')"
+        hero: "linear-gradient(250deg, rgba(130, 201, 30, 0) 0%, #062343 70%), url('/landing.jpeg')"
       }
     }
   },
